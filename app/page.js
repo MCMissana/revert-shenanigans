a130e275ce803fa7b23136442c5ad2d8941454fb
Michael.Missana <MikeyCurtis97654>
2024-01-22 10:10:39 -0500

    another change

--- app/page.js
+++ app/page.js
@@ -4,7 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-     <p>new change</p>
+      <div>
+        <p>new change</p>
+        <p>another change</p>
+      </div>
     </main>
   );
 }
